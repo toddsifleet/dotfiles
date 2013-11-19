2f6b06e7011fa96b4232433470d29acd7246ca69
Todd Sifleet <todd.sifleet@gmail.com>
2013-11-18 21:57:52 -0500

    add sourcing to the begining of file so they can be overidden

--- init.sh
+++ init.sh
@@ -9,11 +9,16 @@ do
   then
     touch $name
   fi
-  echo "source $file" >> $name
+  echo "source $file" >> tmpfile 
+  cat $name >> tmpfile
+  mv tmpfile $name
 done
 
 if [ -f ~/.bash_profile ]
 then
   curl https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh -o $dir/.git-prompt.sh
-  echo "source $dir/.git-prompt.sh" >> ~/.bash_profile
+  echo "source $dir/.git-prompt.sh" >> tmpfile
+  cat ~/.bash_profile >> tmpfile
+  mv tmpfile ~/.bash_profile
 fi
+
