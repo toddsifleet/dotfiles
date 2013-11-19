81c978b7d869e0540bddbde69b50b509aba9181a
Todd Sifleet <todd.sifleet@gmail.com>
2013-11-18 21:11:39 -0500

    Source dotfiles/* at the end of  ~/.<file>
    * Don't move the exisiting files on init
    * append `source $file` to each existing ~/.*
    * won't overwrite machine specific profiles

--- init.sh
+++ init.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 dir=~/dotfiles;
-backup=~/dotfiles_backup;
 
 files="bash_profile vimrc tmux.conf"
 mkdir -p $backup
 for file in $files
 do
-  mv ~/.$file $backup/$file
-  ln -s $dir/$file ~/.$file
+  if [ ! -f "~/.$file" ]
+  then
+    touch ~/.$file
+  fi
+  echo "source ~/dotfiles/$file" >> ~/.$file
 done
