a4eefba8436408484d75098158e3b21609fca01d
Todd Sifleet <todd.sifleet@worldone.com>
2014-04-08 11:22:04 -0400

    add linux support

--- init.sh
+++ init.sh
@@ -1,5 +1,41 @@
 #!/bin/bash
-dir="$( cd "$( dirname "$0" )" && pwd )"
+
+packages=('git' 'vim' 'tmux' 'python' 'ag' 'readline' 'openssl')
+if [ "$(uname)" == "Darwin" ]; then
+  if [ ! "type brew" > /dev/null ]
+  then
+    echo "Installing Homebrew"
+    ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+  fi
+
+  brew doctor
+  for package in "${packages[@]}"
+  do
+    installed=`brew list $package 2> /dev/null`;
+    if [ -z "$installed" ];
+    then
+      echo "Installing $package"
+      brew install $package;
+    fi
+  done
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  apt-get update
+  for package in "{$packages[@]}"
+  do
+    echo "Installing $package"
+    apt-get install $package
+  done
+fi
+
+dir="`pwd`/dotfiles"
+
+if [ ! -d "$dir" ]
+then
+  git clone https://github.com/toddsifleet/dotfiles.git
+else
+  echo "$dir already exists"
+  (cd $dir; git pull)
+fi
 
 for file in "$dir"/*
 do
@@ -18,24 +54,6 @@ do
   fi
 done
 
-if [ ! "type brew" > /dev/null ]
-then
-  echo "Installing Homebrew"
-  ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
-fi
-
-brew doctor
-brew_packages=('git' 'vim' 'tmux' 'python' 'ag' 'readline' 'openssl')
-for package in "${brew_packages[@]}"
-do
-  installed=`brew list $package 2> /dev/null`;
-  if [ -z "$installed" ];
-  then
-    echo "Installing $package"
-    brew install $package;
-  fi
-done
-
 if [ ! -f $dir/.git-prompt.sh ]
 then
   curl https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh -o $dir/.git-prompt.sh
@@ -43,7 +61,6 @@ then
   cat ~/.bash_profile >> tmpfile
   mv tmpfile ~/.bash_profile
 fi
-
 
 if [ ! -d ~/.vim/bundle/vundle ]
 then
